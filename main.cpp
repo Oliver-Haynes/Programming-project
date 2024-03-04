620813944e40ec89e2cf84a0369d079ad2c93d22
Oliver-Haynes <116oliverhaynes@gmail.com>
2024-03-04 05:17:12 +0000

    added pawn setter

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+enum piece_colour{white,black};
+enum piece_type{empty,pawn,knight,bishop,rook,queen,king};
+struct piece{
+    piece_type type;
+    piece_colour colour;
+    std::string icon;
+    std::vector<int> direction;
+};
+
+piece createPawn(piece_colour colour){
+    piece init;
+    init.type=pawn;
+    init.colour = colour;
+    init.icon = (colour==white)?"♙":"♟";
+    init.direction = {1,-1};
+
+    return init;
+}
+
+std::vector<piece> board(64,createPawn(white));
+
+int main(){
+    std::cout<<board[40].type;
+}
